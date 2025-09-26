4ce1c6f9f694603397947b64c0752e2ca5d330ff
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-09-26 15:59:30 +0200

    Update 2_read_pdf.py

--- pages/2_read_pdf.py
+++ pages/2_read_pdf.py
@@ -19,7 +19,8 @@ if file is not None:
 
         match page:
             case 0:
-                race_info = list(re.findall(r'(Session name): (.+) (Session started): (.+) Session time: .+ (Session ended): (.+) Pos', page_text)[0])
+                #race_info = re.findall(r'(Session name): (.+) (Session started): (.+) Session time: .+ (Session ended): (.+) Pos', page_text)
+                race_info = re.findall(r'(Session name)', page_text)
                 st.write(race_info)
 
     # Display the content
