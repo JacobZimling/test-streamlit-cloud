6447a1c267df5426121f74fc2e564c0111b25976
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-09-26 22:16:47 +0200

    Update 2_read_pdf.py

--- pages/2_read_pdf.py
+++ pages/2_read_pdf.py
@@ -22,6 +22,7 @@ if file is not None:
         #content += page_text
 
         if page == 0:
+            st.write(page_text)
             #race_info = re.findall(r'(Session name): (.+) (Session started): (.+)', page_text)
             #st.write(race_info)
             #race_info = re.findall(r'(Session time): (.+) (Session ended): (.+)', page_text)
@@ -40,6 +41,8 @@ if file is not None:
 
         elif page != 1:
             st.write('extract lap times')
+            st.write(page_text)
+
             lap_info = re.findall(r'(\d+) (\w[\w ]+) (\d{2}:\d{2}.\d{3}) (\+.{5}) (\d{2}:\d{2}.\d{3}) (\d+)\.', page_text)
             st.write(lap_info)
 
