3fb045a06de5c4f5aa848c55de04ce97fdc8f925
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-09-26 22:48:28 +0200

    Update 2_read_pdf.py

--- pages/2_read_pdf.py
+++ pages/2_read_pdf.py
@@ -54,7 +54,7 @@ if file is not None:
             st.write(lap_info)
             
             for l in lap_info:
-                params = dict(lap=lap_info[0], driver=lap_info[1])
+                params = dict(lap=lap_info[l][0], driver=lap_info[l][1])
                 st.write(params)
                 
     # Display the content
