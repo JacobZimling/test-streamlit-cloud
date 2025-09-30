0cd2e3b51c1ae49fafa0eafe7b561575f24c5c2d
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-09-30 14:03:30 +0200

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -9,4 +9,4 @@ conn = st.connection('freesqldatabase', type='sql')
 df = conn.query(f"SELECT lap, driver_id, lap_time FROM race_laps WHERE race_id in (14, 15);", ttl=0)
 st.dataframe(df)
 
-st.line_chart(df)
+st.line_chart(df, x='lap_time', y='lap')
