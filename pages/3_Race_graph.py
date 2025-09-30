0a4e5bb5f64e76d40ddffb01ea7c2e6dad8370ba
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-09-30 19:33:14 +0200

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -6,8 +6,8 @@ import streamlit as st
 conn = st.connection('freesqldatabase', type='sql')
 
 # Get lap data
-df = conn.query(f"SELECT lap, driver_id, lap_time FROM race_laps WHERE race_id in (14, 15);", ttl=0)
-# st.dataframe(df)
+df = conn.query(f"SELECT lap, driver_id, lap_time, sum(lap_time) OVER (PARTITION BY driver_id ORDER BY lap) FROM race_laps WHERE race_id in (14, 15);", ttl=0)
+st.dataframe(df)
 
 # st.line_chart(df, x='lap_time', y='lap')
 st.bar_chart(df, x='driver_id', y='lap', sort='driver_id', color='driver_id', stack=False)
