038792847e916fca9bad028e23b65690b8b3c618
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-10-07 16:31:29 +0200

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -2,7 +2,11 @@
 
 import streamlit as st
 
+class Race:
+  def __init__(self):
+    self.conn = st.connection('heliohost', type='sql')
+    
 # Initialize DB connection.
-def db_connect():
-  return st.connection('heliohost', type='sql')
+# def db_connect():
+#   return st.connection('heliohost', type='sql')
   
