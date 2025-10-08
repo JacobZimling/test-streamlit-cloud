8a2d2e0c3e6db5eb072c438719b7bbaf150275aa
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-10-08 13:44:12 +0200

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -2,22 +2,26 @@
 
 import streamlit as st
 
-class Race:
-  def __init__(self):
-    self.conn = st.connection('heliohost', type='sql')
-    # self.races = self.conn.query('SELECT race_date, race_venue, race_name FROM race_info')
-    return
+# class Race:
+  # def __init__(self):
+  #   self.conn = st.connection('heliohost', type='sql')
+  #   # self.races = self.conn.query('SELECT race_date, race_venue, race_name FROM race_info')
+  #   return
 
-  def select_race_year():
-    st.write('Select year')
-    return
+  # def select_race_year():
+  #   st.write('Select year')
+  #   return
+  
+  # def show_races(self):
+  #   st.dataframe(self.races)
+  #   return
+  
+  # def get_race_years(self):
+  #   return self.conn.query('SELECT distinct year(race_date) FROM race_info')
 
-  def show_races(self):
-    st.dataframe(self.races)
-    return
+def db_connect():
+  return st.connection('heliohost', type='sql')
 
-  def get_race_years(self):
-    return self.conn.query('SELECT distinct year(race_date) FROM race_info')
     
     
 
