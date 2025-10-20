85c6c59e386df74ab994d0b83fcbf7d73839802e
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-10-20 16:36:44 +0200

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -22,13 +22,13 @@ import streamlit as st
 def db_connect():
   return st.connection('heliohost', type='sql')
 
-def get_race_years(conn):
-  return conn.query('SELECT distinct year(race_date) as race_year FROM race_info')
+# def get_race_years(conn):
+#   return conn.query('SELECT distinct year(race_date) as race_year FROM race_info')
 
-def get_races(conn, race_year):
-  return conn.query(f'SELECT distinct race_date, race_venue FROM race_info WHERE year(race_date)={race_year}')
+# def get_races(conn, race_year):
+#   return conn.query(f'SELECT distinct race_date, race_venue FROM race_info WHERE year(race_date)={race_year}')
 
-def get_race_info(conn):
-  return 'SELECT race_id, year(race_date) as race_year, date_format(race_date, '%Y-%m-%d') as race_date, race_venue, concat(race_venue, " (", race_date, ")") as venue_label, race_name FROM race_info'
+# def get_race_info(conn):
+#   return 'SELECT race_id, year(race_date) as race_year, date_format(race_date, '%Y-%m-%d') as race_date, race_venue, concat(race_venue, " (", race_date, ")") as venue_label, race_name FROM race_info'
   # return conn.query('SELECT race_id, year(race_date) as race_year, date_format(race_date, '%Y-%m-%d') as race_date, race_venue, concat(race_venue, " (", race_date, ")") as venue_label, race_name FROM race_info')
   
