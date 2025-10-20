8e52fa35a9795e9893be35123a435ffc62b4d1a4
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-10-20 17:10:43 +0200

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -45,8 +45,12 @@ if race_year:
     # st.write(f'{race['race_date']} {race['venue_label']}')
     # race_label[race_selector['race_date']] = race_selector['venue_label']
   
-  for index, race_row in race_selector.itertuples():
+  for index, race_row in race_selector.itertuples(index=False):
     st.write(f'{race_row.race_date} {race_row.venue_label}')
+
+  for index, race_row in race_selector.interrows():
+    st.write(f'{race_row['race_date']} {race_row.['venue_label']}')
+  
   st.write(race_label)
 
   race_venue = st.selectbox('Løbsdag', options=race_selector['venue_label'].unique(), index=None, placeholder='Vælg løbsdag')
