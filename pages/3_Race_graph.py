475333daaa4193e787157334b140a5a7223305ce
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-10-20 15:21:21 +0200

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 # from modules.raceinfo import Race
-from modules import raceinfo as race
+from modules import raceinfo
 
 # Initialize DB connection.
 # conn = st.connection('heliohost', type='sql')
@@ -24,7 +24,7 @@ conn = race.db_connect()
 # https://search.brave.com/search?q=streamlit+scascading+selectboxes+from+dataframe&summary=1&conversation=76d7adeeee7a87c717e6d4
 # https://discuss.streamlit.io/t/format-func-function-examples-please/11295/4
 
-races = race.get_race_info(conn)
+races = raceinfo.get_race_info(conn)
 st.write(races)
 
 # race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år')
