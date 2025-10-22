76211761c8049fbff5a67d6a3b3d38b68fef4196
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-10-22 12:06:19 +0200

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -56,7 +56,7 @@ def get_lap_info(conn, race_date, race_name):
     ) c \
     ORDER BY c.driver_id, c.lap; \
     INSERT INTO temp_heat \
-    SELECT DISTINCT driver_id, 0 as lap, '00:00:00' as lap_time, '00:00:00' as race_time, driver_id as id from temp_heat; \
+    SELECT DISTINCT driver_id, 0 as lap, "00:00:00" as lap_time, "00:00:00" as race_time, driver_id as id from temp_heat; \
     SELECT driver_id, race_time, lap FROM ( \
     	SELECT t.driver_id \
     		,t.race_time \
