b9dfbf65bfe2888385be7fc49fb1b0d32c07f9a8
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-25 15:26:06 +0100

    Create crate_views.sql

---
+++ db/crate_views.sql
@@ -0,0 +1,55 @@
+create view w_race_identifier as
+SELECT  race_id,
+		CONCAT(
+            race_date,
+            race_venue,
+            race_name
+        ) AS race_identifier
+	from race_info;
+	
+create view w_driver_times as
+SELECT t.race_identifier, d.driver_id, t.race_time
+                FROM (
+                    SELECT distinct race_identifier, race_time
+                        FROM race_laps as rl
+                    	join w_race_identifier as wri
+                ) as t
+                JOIN (
+                    SELECT distinct race_identifier, driver_id
+                        FROM race_laps as rl
+                    	join w_race_identifier as wri
+                ) as d
+            	ON t.race_identifier=d.race_identifier;
+		
+create view w_race_graph as		
+SELECT *
+FROM (
+    SELECT 
+    	dt.race_identifier,
+        dt.driver_id, 
+        dt.race_time, 
+        l.lap, 
+        l.race_time AS lap_race_time, 
+/*        dt.race_time - l.race_time AS time_diff,*/
+        ROW_NUMBER() OVER (
+            PARTITION BY dt.race_identifier, dt.driver_id, dt.race_time
+            ORDER BY dt.race_time - l.race_time
+        ) AS rn
+    FROM w_driver_times as dt
+    LEFT JOIN (
+	    SELECT wri.race_identifier, rl.driver_id, rl.race_time, rl.lap
+        FROM race_laps as rl
+        	JOIN w_race_identifier as wri
+        		ON rl.race_id=wri.race_id
+       ) as l
+        ON dt.race_identifier=l.race_identifier
+    	and dt.driver_id = l.driver_id
+        AND dt.race_time >= l.race_time
+) sub
+WHERE 
+
+rn = 1
+/*and 
+race_identifier='2025-06-15Slangerup1' and driver_id='Zimling'
+*/
+ORDER BY driver_id, race_time, lap;
