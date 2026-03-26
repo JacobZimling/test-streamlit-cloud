a62e6e807265fef4fa79245412545ba0f6aee622
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-26 15:50:17 +0100

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -53,7 +53,7 @@ if race_year:
       heat_label[heat_row['race_name']] = heat_name
     # st.write(heat_label)
   
-    race_heat = st.selectbox('Løb', options=heat_selector['race_name'].unique(), index=None, placeholder='Vælg heat', format_func=lambda x: heat_label.get(x), width=300)
+    race_heat = st.selectbox('Løb', options=heat_selector['race_name'].unique(), index=None, placeholder='Vælg løb', format_func=lambda x: heat_label.get(x), width=300)
 
     if race_heat:
       # Get lap data for selected heat
