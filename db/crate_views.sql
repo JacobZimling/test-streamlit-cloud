011a26859efb22619303c0455d45dfe6c848aded
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-26 13:08:38 +0100

    Update crate_views.sql

--- db/crate_views.sql
+++ db/crate_views.sql
@@ -13,11 +13,13 @@ SELECT t.race_identifier, d.driver_id, t.race_time
                     SELECT distinct race_identifier, race_time
                         FROM race_laps as rl
                     	join w_race_identifier as wri
+							ON rl.race_id=wri.race_id
                 ) as t
                 JOIN (
                     SELECT distinct race_identifier, driver_id
                         FROM race_laps as rl
                     	join w_race_identifier as wri
+							ON rl.race_id=wri.race_id
                 ) as d
             	ON t.race_identifier=d.race_identifier;
 		
