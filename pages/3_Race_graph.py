edd7463345ed2d1cb33530fe0879e9e354a7cb19
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-26 14:05:58 +0100

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -12,7 +12,7 @@ conn = race.db_connect()
 
 # Read race information from DB
 races = race.get_race_info(conn)
-st.write(races)
+# st.write(races)
 
 # Select year
 race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år', width=300)
@@ -59,7 +59,7 @@ if race_year:
       # Get lap data for selected heat
       # st.write(f'race_heat {race_heat}')
       df = race.get_lap_info(conn, date_venue, race_heat)
-      # st.dataframe(df)
+      st.dataframe(df)
 
       # st.line_chart(df, x='lap_time', y='lap')
       # st.bar_chart(df, x='driver_id', y='lap', sort='driver_id', color='driver_id', stack=False)
