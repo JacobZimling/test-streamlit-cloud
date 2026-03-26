1bfb633d2adb032e4aec6b976de05c7a7ac749b6
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-26 15:32:14 +0100

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -42,7 +42,6 @@ def race_selector(conn):
   # Read race information from DB
   races = get_race_info(conn)
   # st.write(races)
-  return 'date_venue', 'race_heat'
   
   # Select year
   race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år', width=300)
