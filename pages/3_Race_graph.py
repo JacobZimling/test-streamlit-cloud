9f07adb2582e54ce7e3c48330bcba7f338908e64
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-26 15:24:19 +0100

    Update 3_Race_graph.py

--- pages/3_Race_graph.py
+++ pages/3_Race_graph.py
@@ -15,7 +15,7 @@ st.write(f'date_venue: {date_venue} race_heat: {race_heat}')
 # Get lap data for selected heat
 # st.write(f'race_heat {race_heat}')
 df = race.get_lap_info(conn, date_venue, race_heat)
-# st.dataframe(df)
+st.dataframe(df)
 
 # st.line_chart(df, x='lap_time', y='lap')
 # st.bar_chart(df, x='driver_id', y='lap', sort='driver_id', color='driver_id', stack=False)
