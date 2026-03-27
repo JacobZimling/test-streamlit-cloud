f4e9517136124f556702324f97d0279e407a6d25
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-27 15:23:45 +0100

    Update 2_Read_lapsnapper_pdf_file.py

--- pages/2_Read_lapsnapper_pdf_file.py
+++ pages/2_Read_lapsnapper_pdf_file.py
@@ -105,8 +105,8 @@ if file is not None:
                             dif=lap[3], 
                             rank=lap[5], 
                             race_time=race_time.time().strftime('%H:%M:%S.%f'), 
-                            lap_time_dt=datetime.strptime(lap[2], '%M:%S.%f').strftime('0000-00-00 %H:%M:%S.%f'), 
-                            race_time_dt=race_time.strftime("0000-00-00 %H:%M:%S.%f")
+                            lap_time_dt=datetime.strptime(lap[2], '%M:%S.%f').strftime('%Y-%m-%d %H:%M:%S.%f'), 
+                            race_time_dt=race_time.strftime("%Y-%m-%d %H:%M:%S.%f")
                         )
                     )
                 rt = datetime.strptime(race_result[lap_info[0][1]], '%M:%S.%f')
@@ -120,9 +120,10 @@ if file is not None:
                 query = f"UPDATE race_laps SET race_time=ADDTIME(race_time, '{datetime.strptime(race_result[lap_info[0][1]], '%M:%S.%f')-race_time}') WHERE race_id={race_id} and driver_id='{lap_info[0][1]}';"
                 st.write(query)
                 s.execute(text(query))
+                query = f"UPDATE race_laps SET race_time_dt=ADDTIME(race_time_dt, '{datetime.strptime(race_result[lap_info[0][1]], '%M:%S.%f')-race_time}') WHERE race_id={race_id} and driver_id='{lap_info[0][1]}';"
                 # query = f"UPDATE race_laps SET race_time_dt=DATE_ADD(race_time_dt, INTERVAL '{datetime.strptime(race_result[lap_info[0][1]], '%S.%f')-race_time}') WHERE race_id={race_id} and driver_id='{lap_info[0][1]}';"
-                # st.write(query)
-                # s.execute(text(query))
+                st.write(query)
+                s.execute(text(query))
                 s.commit()
                 
 
