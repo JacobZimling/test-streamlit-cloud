e1dab1be17eb35df2ca2a47afb18e1be49b11dda
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-28 10:53:19 +0100

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -35,7 +35,7 @@ def get_race_info(conn):
   
 def get_lap_info(conn, date_venue, race_name):
   conn.reset()
-  query = f'SELECT * FROM w_race_graph WHERE race_identifier="{date_venue}{race_name}";'
+  query = f'SELECT * FROM race_graph WHERE race_identifier="{date_venue}{race_name}";'
   return conn.query(query, ttl=0)
 
 def format_time(td):
