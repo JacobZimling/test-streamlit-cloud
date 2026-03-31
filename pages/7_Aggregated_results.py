c5195e889d0a33f406d22f063e712300693ade77
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-31 14:21:23 +0200

    Update 7_Aggregated_results.py

--- pages/7_Aggregated_results.py
+++ pages/7_Aggregated_results.py
@@ -6,7 +6,7 @@ conn = race.db_connect()
 
 # Read race information for exlect boxes from DB
 races = race.get_race_info(conn)
-# st.write(type(races))
+st.write(races)
 
 # Select year
 race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år', width=300)
@@ -14,30 +14,35 @@ race_year = st.selectbox('År', options=races['race_year'].unique(), index=None,
 if race_year:
 
     # Select race type (2wd/4wd)
-    race_type = st.selectbox('Løbstype', options=races[races['race_year']==race_year]['race_type'].unique(), index=None, placeholder='Vælg løbstype', width=300)
+    # race_type = st.selectbox('Løbstype', options=races[races['race_year']==race_year]['race_type'].unique(), index=None, placeholder='Vælg løbstype', width=300)
     race_type = st.segmented_control(
         'Løbstype',
         races[races['race_year']==race_year]['race_type'].unique()
     )
 
-    # if race_type:
-    
+    race_type = st.segmented_control(
+        'Løbsdag',
+        races[races['race_year']==race_year]['race_type'].unique()
+    )
 
-    # df = race.get_race_result_aggr(conn, race_year, date_venue, race_heat)
+    st.write(f'{race_type} ')
+
+    # if race_type:
+    #     race_result = race.get_race_result_aggr(conn, race_year, race_type)
     
-#     st.dataframe(
-#         df, 
-#         hide_index=True,
-#         column_config={
-#             "rank": st.column_config.NumberColumn("Placering"),
-#             "driver_id": st.column_config.TextColumn("Kører"),
-#             # "race_time": st.column_config.NumberColumn("Tid", format='%m:%s.%SSS'),
-#             #"race_time": st.column_config.TimeColumn("Tid", format='DD-MM-YYYY HH.mm:ss.SSS'),
-#             # "race_time": st.column_config.NumberColumn("Tid", format='%f'),
-#             # "race_time": st.column_config.TimeColumn("Tid"),
-#             "race_time_dt": st.column_config.TimeColumn("Total tid", format='m:ss.SSS'),
-#             "lap": st.column_config.NumberColumn("Omgange"),
-#             "point": st.column_config.NumberColumn("Point"),
-#         }
-#     )
+    #     st.dataframe(
+    #         race_result, 
+    #         hide_index=True,
+    #         column_config={
+    #             "rank": st.column_config.NumberColumn("Placering"),
+    #             "driver_id": st.column_config.TextColumn("Kører"),
+    #             # "race_time": st.column_config.NumberColumn("Tid", format='%m:%s.%SSS'),
+    #             #"race_time": st.column_config.TimeColumn("Tid", format='DD-MM-YYYY HH.mm:ss.SSS'),
+    #             # "race_time": st.column_config.NumberColumn("Tid", format='%f'),
+    #             # "race_time": st.column_config.TimeColumn("Tid"),
+    #             "race_time_dt": st.column_config.TimeColumn("Total tid", format='m:ss.SSS'),
+    #             "lap": st.column_config.NumberColumn("Omgange"),
+    #             "point": st.column_config.NumberColumn("Point"),
+    #         }
+    #     )
     
