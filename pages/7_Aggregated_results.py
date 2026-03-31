d8582b840f34b70e0fc8f0b4181a10cbc620e970
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-31 13:54:04 +0200

    Update 7_Aggregated_results.py

--- pages/7_Aggregated_results.py
+++ pages/7_Aggregated_results.py
@@ -6,7 +6,7 @@ conn = race.db_connect()
 
 # Read race information for exlect boxes from DB
 races = race.get_race_info(conn)
-st.write(type(races))
+# st.write(type(races))
 
 # Select year
 race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år', width=300)
@@ -14,7 +14,7 @@ race_year = st.selectbox('År', options=races['race_year'].unique(), index=None,
 if race_year:
 
     # Select race type (2wd/4wd)
-    race_type = st.selectbox('Løbstype', options=races['race_type'].unique(), index=None, placeholder='Vælg løbstype', width=300)
+    race_type = st.selectbox('Løbstype', options=races[races['race_year']=race_year]['race_type'].unique(), index=None, placeholder='Vælg løbstype', width=300)
 
     # if race_type:
     
