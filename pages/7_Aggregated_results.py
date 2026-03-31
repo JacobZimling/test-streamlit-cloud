b2162a1fe58b1944b727c73828a0f1c07363159e
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-03-31 13:52:42 +0200

    Update 7_Aggregated_results.py

--- pages/7_Aggregated_results.py
+++ pages/7_Aggregated_results.py
@@ -6,16 +6,19 @@ conn = race.db_connect()
 
 # Read race information for exlect boxes from DB
 races = race.get_race_info(conn)
-st.write(races)
-
-# Select 2wd/4wd
-race_type = st.selectbox('Løbstype', options=races['race_type'].unique(), index=None, placeholder='Vælg løbstype', width=300)
-
+st.write(type(races))
 
 # Select year
-# race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år', width=300)
+race_year = st.selectbox('År', options=races['race_year'].unique(), index=None, placeholder='Vælg år', width=300)
 
-# if race_year:
+if race_year:
+
+    # Select race type (2wd/4wd)
+    race_type = st.selectbox('Løbstype', options=races['race_type'].unique(), index=None, placeholder='Vælg løbstype', width=300)
+
+    # if race_type:
+    
+
 #     df = race.get_race_result_aggr(conn, race_year, date_venue, race_heat)
     
 #     st.dataframe(
