c248d57a231f13d10ae22c46ab40dfff96091758
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-01 15:27:31 +0200

    Update raceinfo.py

--- modules/raceinfo.py
+++ modules/raceinfo.py
@@ -106,14 +106,14 @@ def get_result_identifier(*args):
 	return '¤'.join(list(map(str, args)))
 	
 def get_race_result_aggr(conn, *args):
-	# conn.reset()
-	# result_identifier = get_result_identifier(args)
+	conn.reset()
+	result_identifier = get_result_identifier(args)
 	result_identifier = '¤'.join(list(map(str, args)))
 	query = f'SELECT rank, driver_id, race_time_dt, lap, point FROM w_race_result WHERE result_identifier="{result_identifier}";'
 	st.write(query)
-	# race_result = conn.query(query, ttl=0)
-	# return race_result
-	return 
+	race_result = conn.query(query, ttl=0)
+	return race_result
+	# return 
 
 def race_selector(conn):
   # Read race information from DB
