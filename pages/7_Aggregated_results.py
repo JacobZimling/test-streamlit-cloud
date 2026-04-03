801b509a3d5987447287c131578ba8a54f70fe45
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-03 22:48:29 +0200

    Update 7_Aggregated_results.py

--- pages/7_Aggregated_results.py
+++ pages/7_Aggregated_results.py
@@ -89,6 +89,7 @@ if race_year:
                 "race_time_dt": st.column_config.TimeColumn("Total tid", format='m:ss.SSS'),
                 "lap": st.column_config.NumberColumn("Omgange"),
                 "point": st.column_config.NumberColumn("Point"),
-            }
+            },
+            placeholder=""
         )
     
