4638793c044a4bd59c517c7526114af1a4dc50ea
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-04 19:38:12 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Define visible and invisible pages
-visible_page = st.Page("pages/2_Read_lasnapper_pdf_file.py", title="Read lasnapper pdf file")
+visible_page = st.Page("pages/2_Read_lapsnapper_pdf_file.py", title="Read lapsnapper pdf file")
 invisible_page = st.Page("pages/1_Test_db_connection_to_dreamhost.py", title=" ")  # Empty title hides it
 
 # Create navigation with only visible pages
