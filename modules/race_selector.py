a1cdd7eb0040febe809b596adfdb22158623275a
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-05 11:09:31 +0200

    Update race_selector.py

--- modules/race_selector.py
+++ modules/race_selector.py
@@ -1,2 +1,9 @@
 import streamlit as st
 
+def year_selector(values):
+  race_year = st.segmented_control(
+      'År',
+      values.unique()
+      ,label_visibility = 'collapsed'
+  )
+  return race_year
