726715386030af63083db071a714568f343ac7f8
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-05 11:09:38 +0200

    Update 8_Disqualify_driver.py

--- pages/8_Disqualify_driver.py
+++ pages/8_Disqualify_driver.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from modules import raceinfo as race
+from modules import race_selector as rs
 
 # Initialize DB connection.
 conn = race.db_connect()
@@ -8,3 +9,5 @@ conn = race.db_connect()
 races = race.get_race_info(conn)
 st.write(races)
 
+race_year = rs.year_selector(races['race_year'])
+st.write(race_year)
