d397ad5042b9aab88974947d7bc3883c9539e30c
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-05 18:00:10 +0200

    Update crate_views.sql

--- db/crate_views.sql
+++ db/crate_views.sql
@@ -13,24 +13,6 @@ SELECT  race_id,
 	from race_info;
 
 
-DROP VIEW IF EXISTS w_driver_times;
-create view w_driver_times as
-SELECT t.race_identifier, d.driver_id, t.race_time
-                FROM (
-                    SELECT distinct race_identifier, race_time
-                        FROM race_laps as rl
-                    	join w_race_identifier as wri
-							ON rl.race_id=wri.race_id
-                ) as t
-                JOIN (
-                    SELECT distinct race_identifier, driver_id
-                        FROM race_laps as rl
-                    	join w_race_identifier as wri
-							ON rl.race_id=wri.race_id
-                ) as d
-            	ON t.race_identifier=d.race_identifier;
-		
-
 DROP VIEW IF EXISTS w_race_graph;
 create view w_race_graph as		
 SELECT *
