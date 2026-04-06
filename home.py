0b53f47ae93bd49aaffe66c52d41d43d9036a764
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-06 21:13:14 +0200

    Update home.py

--- home.py
+++ home.py
@@ -7,3 +7,27 @@
 # Create navigation with only visible pages
 #page = st.navigation([visible_page])
 #page.run()   
+
+from st_pages import show_pages, hide_pages, Page
+
+show_pages([
+    Page("home.py"),
+    Page("pages/1_Test_db_connection_to_dreamhost.py"),
+    Page("pages/2_Read_lapsnapper_pdf_file.py"),
+    Page("pages/3_Race_graph.py"),
+    Page("pages/4_Race_results.py"),
+    Page("pages/5_Graph_selector.py"),
+    Page("pages/5_Test.py"),
+    Page("pages/7_Aggregated_results.py"),
+    Page("pages/8_Disqualify_driver.py"),
+])
+
+hide_pages(
+    ["1_Test_db_connection_to_dreamhost.py"],
+    ["4_Race_results.py"],
+    ["5_Graph_selector.py"],
+    ["5_Test.py"],
+    ["8_Disqualify_driver.py"],
+) 
+
+
