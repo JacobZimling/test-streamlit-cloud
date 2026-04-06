f2ffed2e3f3614fdab3cf99308ea82c23df24f66
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2026-04-06 21:43:53 +0200

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import streamlit as st
 pg = st.navigation([
     st.Page(
         "pages/2_Read_lapsnapper_pdf_file.py", 
-        title="Indlæs resultater fr Lapsnapper PDF fil"
+        title="Indlæs resultater fra Lapsnapper PDF fil"
     ),
     st.Page(
         "pages/7_Aggregated_results.py",
