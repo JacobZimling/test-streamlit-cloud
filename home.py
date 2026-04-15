b8879945cc19925f2de82a9ee4657dda4500000f
JacobZimling <contact@zimling.om>
2026-04-15 22:55:13 +0200

    Some pages hidden

--- home.py
+++ home.py
@@ -8,7 +8,8 @@ pg = st.navigation([
     ),
     st.Page(
         "pages/2_Read_lapsnapper_pdf_file.py", 
-        title="Indlæs resultater fra Lapsnapper PDF fil"
+        title="Indlæs resultater fra Lapsnapper PDF fil",
+        visibility="hidden"
     ),
     st.Page(
         "pages/3_Race_graph.py",
@@ -17,39 +18,9 @@ pg = st.navigation([
     st.Page(
         "pages/7_Aggregated_results.py",
         title="Diskvalificering",
-        url_path="Aggregated_results?mode=DSQ"
+        url_path="Aggregated_results?mode=DSQ",
+        visibility="hidden"
     ),
 ])
 pg.run()
-
-# Define visible and invisible pages
-#visible_page = st.Page("pages/2_Read_lapsnapper_pdf_file.py", title="Read lapsnapper pdf file")
-#invisible_page = st.Page("pages/1_Test_db_connection_to_dreamhost.py", title=" ")  # Empty title hides it
-
-# Create navigation with only visible pages
-#page = st.navigation([visible_page])
-#page.run()   
-
-#from st_pages import show_pages, hide_pages, Page
-
-#show_pages([
-#    Page("home.py"),
-#    Page("pages/1_Test_db_connection_to_dreamhost.py"),
-#    Page("pages/2_Read_lapsnapper_pdf_file.py"),
-#    Page("pages/3_Race_graph.py"),
-#    Page("pages/4_Race_results.py"),
-#    Page("pages/5_Graph_selector.py"),
-#    Page("pages/5_Test.py"),
-#    Page("pages/7_Aggregated_results.py"),
-#    Page("pages/8_Disqualify_driver.py"),
-#])
-
-#hide_pages(
-#    ["1_Test_db_connection_to_dreamhost.py"],
-#    ["4_Race_results.py"],
-#    ["5_Graph_selector.py"],
-#    ["5_Test.py"],
-#    ["8_Disqualify_driver.py"],
-#) 
-
 
